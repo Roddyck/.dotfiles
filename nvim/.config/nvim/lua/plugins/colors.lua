d5a7411e779807fe2d8e20726287ce22c21ca21f
Roddyck <rodion.ivachchenko2003@gmail.com>
2024-04-12 21:11:20 +0300

    some changes

--- nvim/.config/nvim/lua/plugins/colors.lua
+++ nvim/.config/nvim/lua/plugins/colors.lua
@@ -18,7 +18,7 @@ return {
                     floats = "dark",   -- style for floating windows
                 },
             })
-
+        vim.cmd.colorscheme "tokyonight"
         end
     },
 
@@ -40,7 +40,7 @@ return {
                     shade = "dark",
                     percentage = 0.15, -- percentage of the shade to apply to the inactive window
                 },
-                no_italic = false,  -- Force no italic
+                no_italic = true,  -- Force no italic
                 no_bold = false,    -- Force no bold
                 no_underline = false, -- Force no underline
                 styles = {          -- Handles the styles of general hi groups (see `:h highlight-args`):
@@ -75,7 +75,7 @@ return {
             })
 
             -- setup must be called before loading
-            vim.cmd.colorscheme "catppuccin"
+            -- vim.cmd.colorscheme "catppuccin"
         end
     }
 }
