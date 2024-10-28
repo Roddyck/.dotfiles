7defa74849a72db4755a42322874b0c3cb8580cc
Roddyck <rodion.ivachchenko2003@gmail.com>
2024-10-28 19:36:23 +0300

    try out wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,25 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Tokyo Night'
+
+config.window_background_opacity = 0.7
+
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
